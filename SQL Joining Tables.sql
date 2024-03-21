1c0d1e101de50221dd780f82b7cd5272843b4666
meaganwindler <130713287+meaganwindler@users.noreply.github.com>
2024-03-21 13:31:44 -0500

    Update SQL Joining Tables.sql

--- SQL Joining Tables.sql
+++ SQL Joining Tables.sql
@@ -1,50 +1,75 @@
+--Sample SQL to demonstrate my ability to join tables and use common table expressions to answer questions about datasets
+
 Use m_windler
 go
 
+-- List the name and truckid for every driver that has made a shipment, along each driverâ€™s average shipment weight.
+SELECT 
+    DriverName, 
+    Truck.TruckID, 
+    AVG(ShipWeight) AS "Average ShipWeight"
+FROM 
+    Truck
+JOIN 
+    Shipment ON Truck.TruckID = Shipment.TruckID
+GROUP BY 
+    DriverName, Truck.TruckID;
 
+-- What are the names of customers who have sent packages (shipments) to Honolulu?
+SELECT 
+    CustName
+FROM 
+    Customer
+JOIN 
+    Shipment ON Customer.CustID = Shipment.CustID
+JOIN 
+    City ON Shipment.CityID = City.CityID
+WHERE 
+    CityName = 'Honolulu';
 
+-- Who are the customers (id, name, and cityname) either: 1) have over $2 million in annual revenue 
+-- and have sent shipments weighing over 900 pounds or 2) have sent a shipment to Lubbock. 
+-- No duplicates please, and alphabetize the city name backwards (Z to A).
+SELECT DISTINCT 
+    Customer.CustID, 
+    CustName, 
+    CityName
+FROM 
+    Customer
+JOIN 
+    Shipment ON Customer.CustID = Shipment.CustID
+JOIN 
+    City ON Shipment.CityID = City.CityID
+WHERE 
+    AnnualRevenue > 2000000 AND ShipWeight > 900 
+    OR CityName = 'Lubbock'
+ORDER BY 
+    CityName DESC;
 
---List the name and truckid for every driver that has made a shipment, along each driver’s average shipment weight.
-select DriverName, Truck.TruckID, avg(ShipWeight) "Average ShipWeight"
-from Truck
-join Shipment on Truck.TruckID = Shipment.TruckID
-group by DriverName,Truck.TruckID;
+-- Who are the drivers (by name) who have delivered shipments for customers with annual 
+-- revenue over $2 million to cities with populations over 4 million?
+SELECT 
+    DriverName
+FROM 
+    Truck
+JOIN 
+    Shipment ON Truck.TruckID = Shipment.TruckID
+JOIN 
+    Customer ON Shipment.CustID = Customer.CustID
+JOIN 
+    City ON Shipment.CityID = City.CityID
+WHERE 
+    AnnualRevenue > 2000000 
+    OR CityPop > 4000000;
 
-
---What are the names of customers who have sent packages (shipments) to Honolulu?
-select CustName
-from Customer
-join Shipment on Customer.CustID = Shipment.CustID
-join City on Shipment.CityID = City.CityID
-where CityName =  'Honolulu';
-
-
---Who are the customers (id, name and cityname) either: 1) have over $2 million in annual revenue 
---and have sent shipments weighing over 900 pounds or 2) have sent a shipment to
---Lubbock. No duplicates please, and alphabetize the city name backwards (Z to A).
-Select distinct Customer.CustID, CustName, CityName
-from Customer
-join Shipment on Customer.CustID = Shipment.CustID
-join City on Shipment.CityID = City.CityID
-where AnnualRevenue > 2000000 and
-ShipWeight > 900 or
-CityName = 'Lubbock'
-Order by CityName desc;
-
-
---Who are the drivers (by name) who have delivered shipments for customers with annual 
---revenue over $2 million to cities with populations over 4 million?
-select DriverName
-From Truck
-join Shipment on Truck.TruckID = Shipment.TruckID
-join Customer on Shipment.CustID = Customer.CustID
-join City on Shipment.CityID = City.CityID
-where AnnualRevenue > 2000000 or 
-CityPop > 4000000;
-
---To what cities have customers with revenue less than $300,000 sent packages?
-select distinct CityName
-from City
-join Shipment on City.CityID = Shipment.CityID
-join Customer on Shipment.CustID = Customer.CustID
-where AnnualRevenue <  300000;
+-- To what cities have customers with revenue less than $300,000 sent packages?
+SELECT DISTINCT 
+    CityName
+FROM 
+    City
+JOIN 
+    Shipment ON City.CityID = Shipment.CityID
+JOIN 
+    Customer ON Shipment.CustID = Customer.CustID
+WHERE 
+    AnnualRevenue < 300000;
